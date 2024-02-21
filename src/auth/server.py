8d2b76571eee689155ccec8e5537e245a8b99d60
Mojtaba Michael <mojimich2015@gmail.com>
2024-02-22 02:59:26 +0300

    Added Kubernetes manifests for auth service

--- src/auth/server.py
+++ src/auth/server.py
@@ -1,6 +1,7 @@
 import jwt, datetime, os
 from flask import Flask, request
 from flask_mysqldb import MySQL
+import logging
 
 server = Flask(__name__)
 mysql = MySQL(server)
@@ -28,11 +29,44 @@ def login():
         return "invalid credentials", 401
     user_row = cur.fetchone()
     email = user_row[0]
-    password  = user_row[1]
+    password = user_row[1]
     if auth.username != email or auth.password != password:
         return "invalid credentials", 401
     return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
 
 
-def createJWT():
-    pass
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+    if not encoded_jwt:
+        return "missing authorization", 401
+    encoded_jwt = encoded_jwt.spit(" ")[1]
+    try:
+        decoded = jwt.decode(
+            encoded_jwt,
+            os.environ.get("JWT_SECRET", "jwt_secret"),
+            algorithm="HS256"
+        )
+    except Exception as e:
+        return "not authorized", 401
+    return decoded, 200
+
+@server.route("/health", methods=["GET"])
+def health():
+    return "Up&Running", 200
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz
+        },
+        secret,
+        algorithm="HS256"
+    )
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
